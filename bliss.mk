f2428a390c0067d5a601967c2834b8671e3dc737
Dan Pasanen <dan.pasanen@gmail.com>
2015-08-25 19:39:07 +0200

    falcon: Specify GMS client id
    Change-Id: I1ddc93474789971d9f20c883ae5921f53451cdc8

--- bliss.mk
+++ bliss.mk
@@ -1,11 +1,9 @@
-#Bliss Audio Mod
-BLISS_AUDIO_MOD := viper
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common Bliss stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := bliss_falcon
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
